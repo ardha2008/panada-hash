fd1581a233e3cb08d519e20886203fde1849df2a
Ardha Herdianto <ardha2008@gmail.com>
2014-07-03 00:45:40 +0700

    Initial Commit
    First release encryption algoritm

---
+++ app/Libraries/panadaHash.php
@@ -0,0 +1,42 @@
+<?php 
+
+
+/**
+*	Panada Hashing
+*	@copy 2014 Ardha Herdianto
+*
+*/
+
+namespace Libraries;
+use Resources;
+
+class panadaHash{
+    
+    function __construct(){
+    	$key='abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()';
+    	$this->crypt=new Resources\Encryption($key);
+    }
+
+    function first($string){
+    	$first=hash('sha256',$string);
+    	return $first;
+    }
+
+    function second($string){
+    	$second=hash('sha1', $string);
+    	return $second;
+    }
+
+    function third($string){
+    	$third=hash('md5',$string);
+    	return $third;
+    }
+    
+    function output($string){
+    	$result=$this->first($string);
+    	$result=$this->second($result);
+    	$result=$this->third($result);
+    	$result=$this->crypt->encrypt($result);
+        return $result;
+    }
+}
